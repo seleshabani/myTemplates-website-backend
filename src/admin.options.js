ecfab9338cf857a550f03b9f7ffefef1afaff02f
seleshabani <seleshabani4@gmail.com>
2021-04-02 23:43:30 +0100

    adminbro loginPage

--- src/admin.options.js
+++ src/admin.options.js
@@ -1,8 +1,39 @@
 const AdminBro = require('admin-bro');
 const AdminBroMongoose = require('@admin-bro/mongoose');
+const bcrypt = require('bcrypt')
 const {item } = require('./model/item');
+const { user } = require('./model/user');
 AdminBro.registerAdapter(AdminBroMongoose)
 const options = {
-    resources: [item],
+    resources: [item,{
+        resource: user,
+        options: {
+          properties: {
+            encryptedPassword: {
+              isVisible: false,
+            },
+            password: {
+              type: 'string',
+              isVisible: {
+                list: false, edit: true, filter: false, show: false,
+              },
+            },
+          },
+          actions: {
+            new: {
+              before: async (request) => {
+                if(request.payload.password) {
+                  request.payload = {
+                    ...request.payload,
+                    encryptedPassword: await bcrypt.hash(request.payload.password, 10),
+                    password: undefined,
+                  }
+                }
+                return request
+              },
+            }
+          }
+        }
+      }],
 };
 module.exports = options;
--- src/router/admin.router.js
+++ src/router/admin.router.js
@@ -1,9 +1,24 @@
 const AdminBro = require('admin-bro');
 const express = require('express');
 const AdminBroExpress = require('@admin-bro/express')
+const {user} = require('../model/user')
+const bcrypt = require('bcrypt')
 
 const buildAdminRouter = (admin)=>{
-    const router = AdminBroExpress.buildRouter(admin);
+    // const router = AdminBroExpress.buildRouter(admin);
+    const router = AdminBroExpress.buildAuthenticatedRouter(admin, {
+        authenticate: async (email, password) => {
+          const userObj = await user.findOne({ email })
+          if (userObj) {
+            const matched = await bcrypt.compare(password, userObj.encryptedPassword)
+            if (matched) {
+              return userObj
+            }
+          }
+          return false
+        },
+        cookiePassword: 'some-secret-password-used-to-secure-cookie'
+    })
     return router;
 }
 
